f1aab76b128fdac437271fd662abd12f91d5d79f
Bartek <piela.bartek@gmail.com>
2017-12-04 09:50:14 +0000

    Changes to test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://buddy.works');
-  await page.screenshot({path: 'example.png'});
+  await page.screenshot({path: 'screen.png'});
 
   await browser.close();
 })();
