0ddd002e48b92fcaf0720e362d7daee19fc80341
Bartek <piela.bartek@gmail.com>
2017-12-04 09:44:35 +0000

    Changes to test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://buddy.works');
-  await page.screenshot({path: 'screen.png'});
+  await page.screenshot({path: 'example.png'});
 
   await browser.close();
 })();
