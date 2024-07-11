c206bfdd4405a82292266d8b47cfa8625b8e84ce
Lucas Czulak <lucas.czulak@buddy.works>
2024-07-11 16:02:08 +0200

    Updated Puppeteer and configured pipeline for screenshots.

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('https://buddy.works');
-  await page.screenshot({path: 'buddy-screenshot.png'});
+  await page.screenshot({ path: 'buddy-screenshot.png' });
 
   await browser.close();
 })();
